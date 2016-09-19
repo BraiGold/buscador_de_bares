ed994fb516a66a5dc5c6801e9d8cf67740ed3ac9
¨camicoy¨ <camicoy94@gmail.com>
2016-09-19 20:27:36 -0300

    se muere mi maquina

--- maintests.py
+++ maintests.py
@@ -4,8 +4,12 @@ from interfazDeUsuario import InterfazDeUsuario
 from interfazMaps import InterfazMaps
 from listaBares import ListaDeBares
 from tests import Tests
-db = "file.txt"
-test = Tests(db)
-test.testAgregar()
-test.testDarDeAlta()
-test.testBuscarBaresCercanos()
+db2 = "file2.txt"
+db = "file1.txt"
+test1 = Tests(db2)
+test1.testAgregar()
+test1.testDarDeAlta()
+test1.testBuscarBaresCercanos()
+
+#test2 = Tests(db)
+#test2.testGlobal()
