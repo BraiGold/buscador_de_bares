c1798850b879184a807b592cb1f446ed743d8d2d
brian goldstein <bgoldstein@dc.uba.ar>
2016-09-19 09:21:39 -0300

    me habia olvidado el main

--- main.py
+++ main.py
@@ -1,49 +1,8 @@
-from bares import *
-
-print 'Bienvenido al buscador de bares'
-
-while 1:
-    print 'Elija una de las siguientes opciones:'
-    print '1-Para agregar un bar.\n2-Para buscar los bares cercanos.\n3-Para dar de alta un bar.\n9-Para salir'
-    op = input()
-    if op == 1:
-        var = 1
-        nom = 'a'
-        dire = 'a'
-        hora = 'a'
-        calif = 1
-        wifi = 1
-        enchuf = 1
-        ruido = 1
-        com = 1
-        aten = 1
-        pre = 1
-        banos = 1
-        agregarBar( var, nom, dire, hora, calif, wifi, enchuf, ruido, com, aten, pre, banos )
-    elif op == 2:
-        print 'Bares cercanos\n\n'
-    elif op == 3:
-        print 'Lista de bares a dar de alta\n\n'
-        i = 1
-        for bar in aDarDeAlta:
-            print i, '  ', bar.nombre, '  ', bar.direccion, '  ', bar.horarios, '  ', bar.general
-            i += 1
-
-        while 1:
-            print '\n\nElija una de los siguientes opciones.'
-            print '1-Dar de alta un bar.\n2-Volver al menu anterior.'
-            op2 = input()
-            if op2 == 1:
-                print 'Ingrese el numero de bar a dar de alta. El numero es la primera columna de la lista.\n'
-                alta = input()
-                darDeAlta( i, alta )
-            elif op2 == 2:
-                break
-            else:
-                print 'La opcion ingresada no existe.\n'
-
-    elif op == 9:
-        print 'Hasta luego'
-        break
-    else:
-        print 'La opcion ingresada no es correcta\n.'
+from bar import Bar
+from interfazBaseDeDatos import InterfazBaseDeDatos
+from interfazDeUsuario import InterfazDeUsuario
+from interfazMaps import InterfazMaps
+from listaBares import ListaDeBares
+db = "file.txt"
+interfazDeUsuario=InterfazDeUsuario(db)
+interfazDeUsuario.menuInicial()
