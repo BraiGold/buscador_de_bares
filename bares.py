10d5de0e36d4751135bb1ee4dcf251606e623be2
¨camicoy¨ <camicoy94@gmail.com>
2016-09-19 01:23:55 -0300

    Cambie mucho las cosas, para poder hacer bien los test

--- bares.py
+++ bares.py
@@ -126,37 +126,3 @@ def agregarBar(  ):
 
     print 'Su bar ha sido agregado.\nEn los proximos dias se procesara la informacion y podra visualizarlo.\n\n'
 
-
-print 'Bienvenido al bucador de bares'
-
-while 1:
-    print 'Elija una de las siguientes opciones:'
-    print '1-Para agregar un bar.\n2-Para buscar los bares cercanos.\n3-Para dar de alta un bar.\n9-Para salir'
-    op = input()
-    if op == 1:
-        agregarBar(  )
-    elif op == 2:
-        print 'Bares cercanos\n\n'
-    elif op == 3:
-        print 'Lista de bares a dar de alta\n\n'
-        i = 1
-        for bar in aDarDeAlta:
-            print i, '  ', bar.nombre, '  ', bar.direccion, '  ', bar.horarios, '  ', bar.general
-            i += 1
-
-        while 1:
-            print '\n\nElija una de los siguientes opciones.'
-            print '1-Dar de alta un bar.\n2-Volver al menu anterior.'
-            op2 = input()
-            if op2 == 1:
-                darDeAlta( i )
-            elif op2 == 2:
-                break
-            else:
-                print 'La opcion ingresada no existe.\n'
-
-    elif op == 9:
-        print 'Hasta luego'
-        break
-    else:
-        print 'La opcion ingresada no es correcta\n.'
---
+++ tests.py
@@ -0,0 +1,3 @@
+from bares import *
+b = Bar('s','s',1,1,1,1,1,1,1,1,1)
+print b.calificacion
