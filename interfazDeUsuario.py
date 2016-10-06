18e4932a02ec1e1c87f700e8cd69b8f82ce6195b
¨camicoy¨ <camicoy94@gmail.com>
2016-10-05 23:57:04 -0300

    interfaz usuario con filtrar

--- interfazDeUsuario.py
+++ interfazDeUsuario.py
@@ -10,7 +10,7 @@ class InterfazDeUsuario:
         print "2. Dar Bar de Alta "
         print "3. Buscar bares cercanos (a menos de 400m) "
         print "4. Mostrar ubicacion de Bar "
-        print "5. Ordenar bares "
+        print "5. Filtrar bares "
         entrada = input()
         if entrada == 1:
             self.menuAgregarBar()
@@ -21,7 +21,7 @@ class InterfazDeUsuario:
         if entrada == 4:
             self.menuMostrarBar()
         if entrada == 5:
-            self.menuOrdenarBares()
+            self.menuFiltrarBares()
         if entrada !=1 and entrada !=2 and entrada !=3:
             self.menuInicial()
 
@@ -50,10 +50,9 @@ class InterfazDeUsuario:
         self.listaDeBares.agregarBar(Bar(nombre,direcc,enchuf,desc, wifi))
         self.menuInicial()
 
-    def menuDarBarDeAlta(self):
-        print "seleccione el numero de bar a dar de alta(-1 para salir): "
+    def mostrarListaDeBares(self, lista):
         numeroDeBar=0
-        for bar in self.listaDeBares.inhabilitados():
+        for bar in lista:
             print "BAR ("+str(numeroDeBar) +"). "
             print "Nombre: "+ bar.nombre() +""
             print "Direccion"+ bar.direccion()+""
@@ -64,6 +63,10 @@ class InterfazDeUsuario:
                 print "WiFi:"+"No" + ""
             print "Descripcion: "+bar.descripcion() +""
             numeroDeBar=numeroDeBar + 1
+
+    def menuDarBarDeAlta(self):
+        print "seleccione el numero de bar a dar de alta(-1 para salir): "
+        self.mostrarListaDeBares(self.listaDeBares.inhabilitados())
         entrada=input()
         if entrada >= 0 and entrada < len(self.listaDeBares.inhabilitados()):
             self.listaDeBares.darDeAlta(self.listaDeBares.inhabilitados()[entrada])
@@ -80,36 +83,33 @@ class InterfazDeUsuario:
 
     def menuMostrarBar(self):
         print "seleccione el numero de bar a mostrar su ubicacion(-1 para salir): "
-        numeroDeBar=0
-        for bar in self.listaDeBares.Habilitados():
-            print "BAR ("+str(numeroDeBar) +"). "
-            print "Nombre: "+ bar.nombre() +""
-            print "Direccion"+ bar.direccion()+""
-            print "Cantidad de Enchufes:"+str(bar.enchufes()) + ""
-            if int(bar.wifi()) == 1:
-                print "WiFi:"+"Si" + ""
-            else:
-                print "WiFi:"+"No" + ""
-            print "Descripcion: "+bar.descripcion() +""
-            numeroDeBar=numeroDeBar + 1
+        self.mostrarListaDeBares(self.listaDeBares.Habilitados())
         entrada=input()
         if entrada >= 0 and entrada < len(self.listaDeBares.Habilitados()):
             self.listaDeBares.mostarBar(self.listaDeBares.Habilitados()[entrada])
         else:
             self.menuInicial()
 
-    def menuOrdenarBares(self):
+    def menuFiltrarBares(self):
         criterios = ["NivelWifi", "NivelRuido", "Atencion", "HigieneBanos", "CalidadComida", "Precios"]
-        print "selcciones los dos criterios por los que se ordenara"
+        print "selcciones los dos criterios por los que se filtrara(-1 para salir)"
         print "0. NivelWifi", "1. NivelRuido", "2. Atencion", "3. HigieneBanos", "4. CalidadComida", "5. Precios"
         print "Ingrese el primer criterio"
         criterio1 = input()
         if criterio1 >= 0 and criterio1 <= 5:
+            print "Ingrese la cantidad de estrellas minimas para el primer criterio(entre 1 y 5)"
+            valor1 = input()
+            if valor1 < 1 or valor1 > 5:
+                self.menuFiltrarBares()
             print "Ingrese el segundo criterio"
             criterio2 = input()
             if criterio2 >= 0 and criterio2 <= 5 and criterio2 != criterio1:
-                self.listaDeBares.ordenarBaresPor(criterios[criterio1],criterios[criterio2])
+                print "Ingrese la cantidad de estrellas minimas para el segundo criterio(entre 1 y 5)"
+                valor2 = input()
+                if valor2 < 1 or valor2 > 5:
+                    self.menuFiltrarBares()
+                self.listaDeBares.ordenarBaresPor(criterios[criterio1],valor1,criterios[criterio2],valor2)
             else:
-                self.menuInicial()
+                self.menuFiltrarBares()
         else:
             self.menuInicial()
